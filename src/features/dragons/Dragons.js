172d29bbce7346980dd05730205839fa5bd72132
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-30 19:21:01 +0200

    Conditional classes based on Dragon state

--- src/features/dragons/Dragons.js
+++ src/features/dragons/Dragons.js
@@ -1,29 +1,28 @@
 import "./Dragons.css";
 const Dragons = () => {
+  const isReserved = false;
   return (
-    <div class="container">
-      <div class="card">
-        <div class="container flex gap-2">
-          <img src="./img/9fWdwNv.jpg" alt="" class="card__img" />
-          <div class="card__text flex portrait gap-1">
-            <h4 class="card__heading">Falcon 1</h4>
-            <p>"type": "capsule"</p>
-            <button class="btn btn--un-reserved no-stretch">
-              Reserve Dragon
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="container flex gap-2">
-          <img src="./img/9fWdwNv.jpg" alt="" class="card__img" />
-          <div class="card__text flex portrait gap-1">
-            <h4 class="card__heading no-stretch">
-              <span class="label label--reserved txt-center">Reserved</span>
-              Falcon 2{" "}
+    <div className="container">
+      <div className="card">
+        <div className="container flex gap-2">
+          <img src="./img/9fWdwNv.jpg" alt="" className="card__img" />
+          <div className="card__text flex portrait gap-1">
+            <h4 className="card__heading no-stretch">
+              {isReserved ? (
+                <span className="label label--reserved txt-center">
+                  Reserved
+                </span>
+              ) : null}
+              Falcon 2
             </h4>
             <p>"type": "capsule"</p>
-            <button class="btn btn--reserved ">Cancel Reservation</button>
+            <button
+              className={`btn ${
+                isReserved ? "btn--reserved" : "btn--un-reserved"
+              }`}
+            >
+              Cancel Reservation
+            </button>
           </div>
         </div>
       </div>
