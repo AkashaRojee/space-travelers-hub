9122432d2b0242f59d06c291cf160a7a76ebc3f0
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-30 14:52:59 +0200

    Add main styles [nav] && Add Active class

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo  from './assets/image/planet.svg'
+import logo  from './assets/image/planet.svg';
+import './header.css'
 
 const linkList = [
   { id: 1, name: 'Rockets', to: '/'},
   { id: 2, name: 'Dragons', to: '/dragons'},
   { id: 3, name: 'Missions', to: '/missions'},
-  { id: 4, name: 'My profil', to: '/profil'},
+  { id: 4, name: 'My profile', to: '/profile'},
 ]
 
 const Header = () => {
   return (
-    <header>
-        <div className="logo-wrapper">
-          <img src={logo} alt="logo" />
-        </div>
-        <nav className="nav-bar">
-          <ul className="nav-list">
+    <header class='header'>
+      <div class='container flex  space-between cross-center gap-2 '>
+        <img src={logo} alt="logo" class='logo' />
+        <nav className="nav-bar container ">
+          <ul className="nav-list flex  cross-center gap-3">
             {
               linkList.map(({ id, name, to }) => (
             <li key={id} className="nav-link">
-              <NavLink to={to}>{name}</NavLink>
+              <NavLink activeClassName='active' exact={true} to={to}>{name}</NavLink>
             </li>
               ))
             }
           </ul>
         </nav>
+      </div>
     </header>
   );
 };
