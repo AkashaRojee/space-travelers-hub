a9924b24f85bc61b732fc90249cc5b50737ebbdb
moise10r <moiserushanika2006@gmail.com>
2021-08-31 00:45:49 +0200

    Add the table mission layout

--- src/features/missions/Missions.js
+++ src/features/missions/Missions.js
@@ -2,18 +2,43 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from './missionsSlice';
 const Missions = () => {
-  const dispatch =  useDispatch();
-  const { missions } =  useSelector((state) => state).missions;
-  console.log(missions);
-  useEffect(() => {
-    dispatch(getMissions())
-  }, [dispatch]);
-  return (<div>
-    {
-      missions.map((mission) => <p key={mission.mission_id}>{mission.description}</p>)
-    }
-    </div>)
-}
-
+	const dispatch = useDispatch();
+	const { missions } = useSelector((state) => state).missions;
+	console.log(missions);
+	useEffect(() => {
+		dispatch(getMissions());
+	}, [dispatch]);
+	return (
+		<section className='missions-section'>
+			<table className="mission-table">
+				<thead>
+					<tr>
+						<th>Missions</th>
+						<th>Descripition</th>
+						<th>Status</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              porro necessitatibus maiores sequi. Dicta libero facere iste
+              adipisci odit recusandae quae fuga impedit cupiditate, sint deleniti
+              laboriosam
+            </td>
+            <td>
+              <span>Not a member</span>
+            </td>
+            <td>
+              <button>Join Mission</button>
+            </td>
+          </tr>
+        </tbody>
+			</table>
+		</section>
+	);
+};
 
 export default Missions;
