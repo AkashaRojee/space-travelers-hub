764cf9b535e91dc3e64686b2fc4ae734fc87f62b
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-30 19:06:54 +0200

    Add Dragons component with styles

--- src/features/dragons/Dragons.js
+++ src/features/dragons/Dragons.js
@@ -1,5 +1,34 @@
-const Dragons = () => 
-  <p>dragons</p>
-
+import "./Dragons.css";
+const Dragons = () => {
+  return (
+    <div class="container">
+      <div class="card">
+        <div class="container flex gap-2">
+          <img src="./img/9fWdwNv.jpg" alt="" class="card__img" />
+          <div class="card__text flex portrait gap-1">
+            <h4 class="card__heading">Falcon 1</h4>
+            <p>"type": "capsule"</p>
+            <button class="btn btn--un-reserved no-stretch">
+              Reserve Dragon
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="container flex gap-2">
+          <img src="./img/9fWdwNv.jpg" alt="" class="card__img" />
+          <div class="card__text flex portrait gap-1">
+            <h4 class="card__heading no-stretch">
+              <span class="label label--reserved txt-center">Reserved</span>
+              Falcon 2{" "}
+            </h4>
+            <p>"type": "capsule"</p>
+            <button class="btn btn--reserved ">Cancel Reservation</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dragons;
