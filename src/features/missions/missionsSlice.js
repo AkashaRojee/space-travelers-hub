c59e4efbbb635a2a453ac532223c3f5ed8f23948
moise10r <moiserushanika2006@gmail.com>
2021-08-30 23:48:05 +0200

    Create the mission slice

--- src/features/missions/missionsSlice.js
+++ src/features/missions/missionsSlice.js
@@ -5,3 +5,27 @@ const getMissions = createAsyncThunk('/missions/getMissions', async () => {
     const { data } = await axios.get('https://api.spacexdata.com/v3/missions');
       return data
   });
+  
+  const missionSlice =  createSlice({
+    name: 'missons',
+    initialState: {
+      missons: [],
+      status: null,
+    },
+    status: null,
+    extraReducers: {
+      [getMissions.pending]: (state) => {
+        state.status = 'loading';
+      },
+      [getMissions.fulfilled]: (state, { payload }) => {
+        state.missions = payload;
+        state.status = 'sucess';
+      },
+      [getMissions.rejected]: (state) => {
+        state.status = 'failed';
+      },
+    }
+  });
+  
+  
+  export default missionSlice.reducer;
