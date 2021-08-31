63bcffcedcbcb60e2127ac604b2a2795db8d1b6d
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-31 06:59:50 +0200

    Add action types for dragon-store && handle DRAGON_REQUESTED action

--- src/features/dragons/store/dragonsSlice.js
+++ src/features/dragons/store/dragonsSlice.js
@@ -1,3 +1,10 @@
+// ACTION TYPES
+
+const DRAGONS_REQUESTED = 'dragons/request';
+const DRAGONS_LOADED = 'dragons/load';
+const DRAGON_RESERVED = 'dragon/:id/reserve';
+const DRAGON_UNRESERVED = 'dragon/:id/unreserve';
+
 // REDUCER
 
 const initialState = {
@@ -6,7 +13,8 @@ const initialState = {
 };
 const reducer = (state = initialState, action) => {
   const { type, payload } = action;
-  return 'let there be light';
+  if (type === DRAGONS_REQUESTED) return { ...state, loading: true };
+  return state;
 };
 
 export default reducer;
