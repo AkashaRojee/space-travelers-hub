265cf3f3d71249be7bef773d47dacae7780649d4
moise10r <moiserushanika2006@gmail.com>
2021-08-31 20:28:03 +0200

    Change the text dynamically

--- src/features/missions/Missions.js
+++ src/features/missions/Missions.js
@@ -38,10 +38,10 @@ const Missions = () => {
 								{mission.description}
 							</td>
 							<td>
-								<span className={`${ mission.joined ? 'joined' : ''}`} >Not a member</span>
+								<span className={`${ mission.joined ? 'joined' : ''}`} >{!mission.joined ? 'NOT A MEMBER': 'Active Member' }</span>
 							</td>
 							<td>
-								<button className={`${ mission.joined ? 'joined' : ''}`} onClick={() => handleJoinMission(mission.mission_id, mission.joined )}>Join Mission</button>
+								<button className={`${ mission.joined ? 'joined' : ''}`} onClick={() => handleJoinMission(mission.mission_id, mission.joined )}>{!mission.joined ? 'Join Mission' : 'leave Mission' }</button>
 							</td>
 						</tr>
 						))
