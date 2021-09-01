0a3fd57b21655a98c35a66b5a3c1270640da1a20
moise10r <moiserushanika2006@gmail.com>
2021-09-01 11:01:01 +0200

    props type validation

--- src/app/common/missionTableBody.js
+++ src/app/common/missionTableBody.js
@@ -1,31 +1,29 @@
-/* eslint-disable react/no-typos */
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
-const TableBody = ({ missions, onJoinMission }) => {
-  return (
-    <tbody>
+const TableBody = ({ missions, onJoinMission }) => (
+  <tbody>
     {
       missions.map((mission) => (
         <tr key={mission.mission_id}>
-        <td>{mission.mission_name}</td>
-        <td>
-          {mission.description}
-        </td>
-        <td>
-          <span className={`${ mission.joined ? 'joined' : ''}`} >{!mission.joined ? 'NOT A MEMBER': 'Active Member' }</span>
-        </td>
-        <td>
-          <button className={`${ mission.joined ? 'joined' : ''}`} onClick={() => onJoinMission(mission.mission_id, mission.joined )}>{!mission.joined ? 'Join Mission' : 'leave Mission' }</button>
-        </td>
-      </tr>
+          <td>{mission.mission_name}</td>
+          <td>
+            {mission.description}
+          </td>
+          <td>
+            <span className={`${mission.joined ? 'joined' : ''}`}>{!mission.joined ? 'NOT A MEMBER' : 'Active Member' }</span>
+          </td>
+          <td>
+            <button type="button" className={`${mission.joined ? 'joined' : ''}`} onClick={() => onJoinMission(mission.mission_id, mission.joined)}>{!mission.joined ? 'Join Mission' : 'leave Mission' }</button>
+          </td>
+        </tr>
       ))
     }
   </tbody>
-  );
+);
+
+TableBody.propTypes = {
+  missions: propTypes.arrayOf(propTypes.string).isRequired,
+  onJoinMission: propTypes.func.isRequired,
 };
-TableBody.PropTypes = {
-  mission: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onJoinMission: PropTypes.func.isRequired,
-}
 export default TableBody;
