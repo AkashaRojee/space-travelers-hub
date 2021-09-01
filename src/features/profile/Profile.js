45ce0b76410b9bc50a79a504c051e9c6c29978e7
moise10r <moiserushanika2006@gmail.com>
2021-09-01 11:01:53 +0200

    Fix indentation space issue in my profile

--- src/features/profile/Profile.js
+++ src/features/profile/Profile.js
@@ -2,15 +2,16 @@ import { useSelector } from 'react-redux';
 
 const Profiles = () => {
   const { missions } = useSelector((state) => state).missions;
-  const joinedMission = missions.filter((mission) => mission.joined === true);  
-  return (<div>
-    {
+  const joinedMission = missions.filter((mission) => mission.joined === true);
+  return (
+    <div>
+      {
       joinedMission.map((mission) => (
         <p key={mission.mission_id}>{mission.mission_name}</p>
       ))
     }
-  </div>)
-}
-
+    </div>
+  );
+};
 
 export default Profiles;
