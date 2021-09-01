6f97f18a5027c3b6866e9f1e74127d871a9875d1
moise10r <moiserushanika2006@gmail.com>
2021-09-01 10:50:18 +0200

    Fix indentation space issue in tablebody

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo  from './planet.svg';
+import logo from './planet.svg';
 import './header.css';
 
 const linkList = [
-  { id: 1, name: 'Rockets', to: '/'},
-  { id: 2, name: 'Dragons', to: '/dragons'},
-  { id: 3, name: 'Missions', to: '/missions'},
-  { id: 4, name: 'My profile', to: '/profile'},
-]
+  { id: 1, name: 'Rockets', to: '/' },
+  { id: 2, name: 'Dragons', to: '/dragons' },
+  { id: 3, name: 'Missions', to: '/missions' },
+  { id: 4, name: 'My profile', to: '/profile' },
+];
 
-const Header = () => {
-  return (
-    <header className='header'>
-      <div className='container flex  space-between cross-center gap-2 '>
-        <img src={logo} alt="logo" className='logo' />
-        <nav className="nav-bar container ">
-          <ul className="nav-list flex  cross-center gap-3">
-            {
+const Header = () => (
+  <header className="header">
+    <div className="container flex  space-between cross-center gap-2 ">
+      <img src={logo} alt="logo" className="logo" />
+      <nav className="nav-bar container ">
+        <ul className="nav-list flex  cross-center gap-3">
+          {
               linkList.map(({ id, name, to }) => (
-            <li key={id} className="nav-link">
-              <NavLink activeClassName='active' exact={true} to={to}>{name}</NavLink>
-            </li>
+                <li key={id} className="nav-link">
+                  <NavLink activeClassName="active" exact to={to}>{name}</NavLink>
+                </li>
               ))
             }
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
 
 export default Header;
