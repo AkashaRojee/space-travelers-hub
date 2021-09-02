e9f07b09f6373d66be6f09289bc1193e2ee3d064
Akasha <Akasha.Rojee@gmail.com>
2021-09-02 16:29:50 +0400

    Use createAsyncThunk for getting rockets

---
+++ src/__tests__/test-utils.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render as rtlRender } from '@testing-library/react'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import userReducer from '../features/rockets/rocketSlice'
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { user: userReducer }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+}
+
+// re-export everything
+export * from '@testing-library/react'
+// override render method
+export { render }
--- src/features/rockets/rocketSlice.js
+++ src/features/rockets/rocketSlice.js
@@ -1,13 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+export const getRockets = createAsyncThunk('/rockets/get', async () => {
+  const { data } = await axios.get('https://api.spacexdata.com/v3/rockets');
+  return data;
+});
 
 export const rocketSlice = createSlice({
   name: 'rockets',
   initialState: [],
   reducers: {
-    add: (state, action) => {
-      state.push(action.payload);
-    },
     reserve: (state, action) => {
       state.find((rocket) => rocket.id === action.payload).reserved = true;
     },
@@ -15,20 +17,20 @@ export const rocketSlice = createSlice({
       state.find((rocket) => rocket.id === action.payload).reserved = false;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(getRockets.fulfilled, (state, action) => {
+      action.payload.forEach((rocket) => {
+        state.push({
+          id: rocket.rocket_id,
+          title: rocket.rocket_name,
+          description: rocket.description,
+          image: rocket.flickr_images[0],
+        });
+      });
+    });
+  },
 });
 
 export const { add, reserve, cancel } = rocketSlice.actions;
-
-export const getRockets = () => async (dispatch) => {
-  const { data } = await axios.get('https://api.spacexdata.com/v3/rockets');
-  data.forEach((rocket) => {
-    dispatch(add({
-      id: rocket.rocket_id,
-      title: rocket.rocket_name,
-      description: rocket.description,
-      image: rocket.flickr_images[0],
-    }));
-  });
-};
 
 export default rocketSlice.reducer;
